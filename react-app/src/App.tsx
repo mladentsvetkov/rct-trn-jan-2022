42bd2183186b92a335cbe50d16ae4885046cfa4f
Mladen Tsvetkov <mladen.tsvetkov@amdaris.com>
2022-12-11 09:46:14 +0200

    setup done

---
+++ react-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Alert } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+const App = () => {
+  const { data } = useQuery(['use-query-test'], () =>
+    axios('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5')
+  )
+
+  // let a = 3
+
+  // let b: boolean = a
+
+  // console.log(b)
+
+  console.log('hello')
+
+  return (
+    <div>
+      <Alert color="warning">Warning! This is my first MUI component instance :)</Alert>
+      <br />
+      <br />
+      <div>This is a random response to test react query and axios: {data?.data}</div>
+      <br />
+      <br />
+      <Link to="/somepage">Let's test this dom router.</Link>
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/src/pages/SomePage.tsx
@@ -0,0 +1,1 @@
+export const SomePage = () => <>Hello there!</>
